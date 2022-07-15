d305a824f25f9d77efb8a8ac765045a8f1b3bf65
Eason <52824126+Yukino-3@users.noreply.github.com>
2022-07-15 17:24:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ is_conti = False
 conti_path = './CP_dir/aia_merge_new/checkpoint_early_exit_46th.pth.tar'
 is_pesq =  True #use pesq criterion for validate or not
 # server parameter settings
-json_dir = '/home/yuguochen/vbdata/Json'
-file_path = '/home/yuguochen/vbdataset'
-loss_dir = './LOSS/XXX.mat'
-batch_size = 4
+json_dir = 'your path'
+file_path = 'your path'
+loss_dir = 'your path'
+batch_size = 16
 epochs = 80
-lr = 5e-4
+lr = 8e-4
 model_best_path = './BEST_MODEL/XXX.pth.tar'
 check_point_path = './CP_dir/XXX'
 
